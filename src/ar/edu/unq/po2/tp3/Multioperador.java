706ecff66a10bafbdbd52049d1ec3aad80ee6a66
Franco Marengo <francomarengo.21@gmail.com>
2022-04-16 09:55:13 -0300

    Finalizacion de ejercicio 3 con sus respectivos tests

--- src/ar/edu/unq/po2/tp3/Multioperador.java
+++ src/ar/edu/unq/po2/tp3/Multioperador.java
@@ -3,12 +3,33 @@ import java.util.ArrayList;
 
 public class Multioperador {
     
+	private ArrayList<Integer> arrayDeNumeros = new ArrayList<Integer>();
+	
+	public void agregarAlArray(int numero) {
+		arrayDeNumeros.add(numero);
+	}
 
-	public int sumaArray(ArrayList<Integer> lista) {
+	public int sumaArray() {
 		int sumaHastaAhora = 0;
-		for (int numero:lista) {
+		for (int numero:arrayDeNumeros) {
 			sumaHastaAhora = sumaHastaAhora + numero;
 		}
 		return sumaHastaAhora;
+	}
+	
+	public int restaArray() {
+		int restaHastaAhora = 0;
+		for (int numero:arrayDeNumeros) {
+			restaHastaAhora = restaHastaAhora - numero;
+		}
+		return restaHastaAhora;
+	}
+	
+	public int multiplicacionArray() {
+		int multiplicacionHastaAhora = 1;
+		for (int numero:arrayDeNumeros) {
+			multiplicacionHastaAhora = multiplicacionHastaAhora * numero;
+		}
+		return multiplicacionHastaAhora;
 	}
 }
--- test/MultioperadorTestCase.java
+++ test/MultioperadorTestCase.java
@@ -11,5 +11,36 @@ public class MultioperadorTestCase {
 	@BeforeEach
 	public void setUp() throws Exception {
 		
+		// Se crea un nuevo contador
+		multioperador = new Multioperador();
+		
+		// Se agregan al array 5 números
+		multioperador.agregarAlArray(1);
+		multioperador.agregarAlArray(2);
+		multioperador.agregarAlArray(3);
+		multioperador.agregarAlArray(4);
+		multioperador.agregarAlArray(5);
+		
+	}
+	
+	@Test
+	public void sumaMultioperadorTestCase() {
+		int sumaFinal = multioperador.sumaArray();
+		
+		assertEquals(sumaFinal, 15);
+	}
+	
+	@Test
+	public void restaMultioperadorTestCase() {
+		int restaFinal = multioperador.restaArray();
+		
+		assertEquals(restaFinal, -15);
+	}
+	
+	@Test
+	public void multiplicacionMultioperadorTestCase() {
+		int multiplicacionFinal = multioperador.multiplicacionArray();
+		
+		assertEquals(multiplicacionFinal, 120);
 	}
 }
